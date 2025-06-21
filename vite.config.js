27367a89cb58e3e59d7676ae1de45306d7931792
PauTurro <pauturro25@gmail.com>
2025-06-21 17:52:12 +0200

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/STARR/', // 👈 required for GitHub Pages
+  base: '/Starry/', // 👈 required for GitHub Pages
   plugins: [react()],
   server: {
     host: true, // 👈 allows access from phone via local IP
